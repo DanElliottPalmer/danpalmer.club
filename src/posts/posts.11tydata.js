8628549664329b41195423af8c8def60f7580f58
danelliottpalmer <danelliottpalmer@gmail.com>
2021-11-30 22:07:03 -0500

    feat(layouts): add json-ld schema to posts

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,12 +1,33 @@
 const { format } = require("date-fns");
 const slugify = require('slugify');
 
+function getPostSchema(data){
+  const url = new URL(data.page.url, `https://danpalmer.club`);
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    "author": {
+      "@type": "Person",
+      "name": "Dan Palmer"
+    },
+    "datePublished": "2015-02-05T08:00:00+08:00",
+    "headline": data.title,
+    "mainEntityOfPage": url.toString(),
+    "url": url.toString()
+  };
+
+  if(data.description) schema.abstract = data.description;
+
+  return JSON.stringify(schema);
+}
+
 module.exports = {
   eleventyComputed: {
     category_name: data => data.category,
     category_url: data => `/categories/#${slugify(data.category, {lower:true})}`,
     publish_date_label: data => format(data.date, "do MMM, y"),
     publish_date_datetime: data => data.date,
+    schema: getPostSchema,
     tags: data => {
       if(!data.tags) return [];
       return data.tags.map(tag => {
