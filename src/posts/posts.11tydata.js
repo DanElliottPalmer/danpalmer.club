d0d579e4ecefe5366853191edb6735dfbe3f9d6d
danelliottpalmer <danelliottpalmer@gmail.com>
2021-11-30 22:32:19 -0500

    style(prettier): all the src files

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,41 +1,42 @@
 const { format } = require("date-fns");
-const slugify = require('slugify');
+const slugify = require("slugify");
 
-function getPostSchema(data){
+function getPostSchema(data) {
   const url = new URL(data.page.url, `https://danpalmer.club`);
   const schema = {
     "@context": "https://schema.org",
     "@type": "BlogPosting",
-    "author": {
+    author: {
       "@type": "Person",
-      "name": "Dan Palmer"
+      name: "Dan Palmer",
     },
-    "datePublished": "2015-02-05T08:00:00+08:00",
-    "headline": data.title,
-    "mainEntityOfPage": url.toString(),
-    "url": url.toString()
+    datePublished: "2015-02-05T08:00:00+08:00",
+    headline: data.title,
+    mainEntityOfPage: url.toString(),
+    url: url.toString(),
   };
 
-  if(data.description) schema.abstract = data.description;
+  if (data.description) schema.abstract = data.description;
 
   return JSON.stringify(schema);
 }
 
 module.exports = {
   eleventyComputed: {
-    category_name: data => data.category,
-    category_url: data => `/categories/#${slugify(data.category, {lower:true})}`,
-    publish_date_label: data => format(data.date, "do MMM, y"),
-    publish_date_datetime: data => data.date,
+    category_name: (data) => data.category,
+    category_url: (data) =>
+      `/categories/#${slugify(data.category, { lower: true })}`,
+    publish_date_label: (data) => format(data.date, "do MMM, y"),
+    publish_date_datetime: (data) => data.date,
     schema: getPostSchema,
-    tags: data => {
-      if(!data.tags) return [];
-      return data.tags.map(tag => {
+    tags: (data) => {
+      if (!data.tags) return [];
+      return data.tags.map((tag) => {
         return {
           label: tag,
-          url: `/tags/#${slugify(tag, {lower:true})}`
+          url: `/tags/#${slugify(tag, { lower: true })}`,
         };
       });
-    }
-  }
+    },
+  },
 };
